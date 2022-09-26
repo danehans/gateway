5eec8e4f15b1a5c79ab4bfef377afe4dbce5de6e
danehans <daneyonhansen@gmail.com>
2022-09-26 12:46:42 -0700

    WIP: Conformance Tests
    Signed-off-by: danehans <daneyonhansen@gmail.com>

--- test/conformance/conformance_test.go
+++ test/conformance/conformance_test.go
@@ -10,7 +10,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/gateway-api/apis/v1alpha2"
-	"sigs.k8s.io/gateway-api/conformance/tests"
 	"sigs.k8s.io/gateway-api/conformance/utils/flags"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
 )
@@ -25,12 +24,18 @@ func TestGatewayAPIConformance(t *testing.T) {
 	require.NoError(t, v1alpha2.AddToScheme(client.Scheme()))
 
 	cSuite := suite.New(suite.Options{
-		Client:               client,
+		Client:        client,
+		BaseManifests: "https://raw.githubusercontent.com/danehans/gateway/conformance_tests/internal/provider/kubernetes/config/samples/conformance.yaml",
+		//ValidUniqueListenerPorts: []gwapiv1a2.PortNumber{gwapiv1a2.PortNumber(int32(80)), gwapiv1a2.PortNumber(int32(81)), gwapiv1a2.PortNumber(int32(82)), gwapiv1a2.PortNumber(int32(83)), gwapiv1a2.PortNumber(int32(84))},
 		GatewayClassName:     *flags.GatewayClassName,
 		Debug:                *flags.ShowDebug,
 		CleanupBaseResources: *flags.CleanupBaseResources,
+		//ExemptFeatures:           []suite.ExemptFeature{suite.ExemptReferenceGrant, "TLSRoute"},
 	})
 	cSuite.Setup(t)
-	cSuite.Run(t, tests.ConformanceTests)
+
+	//egTests := []suite.ConformanceTest{tests.HTTPRouteCrossNamespace}
+	egTests := []suite.ConformanceTest{tests.HTTPRouteSimpleSameNamespace}
+	cSuite.Run(t, egTests)
 
 }
