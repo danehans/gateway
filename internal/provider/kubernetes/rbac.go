ba29115f121f212db10a49eaa73cc13eadf838d0
Arko Dasgupta <arko@tetrate.io>
2023-01-13 20:57:47 -0800

    Add RateLimitFilter support in k8s provider
    Relates to https://github.com/envoyproxy/gateway/issues/670
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- internal/provider/kubernetes/rbac.go
+++ internal/provider/kubernetes/rbac.go
@@ -7,7 +7,7 @@ package kubernetes
 
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses;gateways;httproutes;grpcroutes;tlsroutes;tcproutes;udproutes;referencepolicies;referencegrants,verbs=get;list;watch;update
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses/status;gateways/status;httproutes/status;grpcroutes/status;tlsroutes/status;tcproutes/status;udproutes/status,verbs=update
-// +kubebuilder:rbac:groups="gateway.envoyproxy.io",resources=authenticationfilters,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups="gateway.envoyproxy.io",resources=authenticationfilters;ratelimitfilters,verbs=get;list;watch;update
 
 // RBAC for watched resources of Gateway API controllers.
 // +kubebuilder:rbac:groups="",resources=secrets;services;namespaces,verbs=get;list;watch
